93e328963fa903308cfcd5d50b35ae0d4a69fb8c
AdvicSaha443 <amazingadvicsaha443@gmail.com>
2023-11-13 15:37:00 +0530

    basic updates

--- public/js/web_player.js
+++ public/js/web_player.js
@@ -1,8 +1,36 @@
 function checkIfLogged(){
-    if(window.localStorage.getItem('access_token') != undefined){
-        
-    }else{
+    if(window.localStorage.getItem('access_token') == undefined){
         alert("You aren't logged in!");
         window.location.replace("http://localhost:3000/login");
-    }
+    };
+};
+
+function getAccessCode(){
+    checkIfLogged();
+    return window.localStorage.getItem('access_token');
+};
+
+function getNewAccessToken(){
+    //code to get a new access token
+};
+
+
+async function fetchWebApi(endpoint, method, body){
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+        headers: {
+            Authorization: `Bearer ${getAccessCode()}`
+        },
+        method: method,
+        body: JSON.stringify(body),
+    });
+
+    return await res.json();
+}
+
+async function getUserInformation(){
+    return await fetchWebApi('v1/me', "GET");
+}
+
+async function updateInformation(){
+    
 }
